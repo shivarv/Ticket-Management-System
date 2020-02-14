622ca7f3019697713581274334e7148887e63d5b
shiva_rv <shivarv1993@gmail.com>
2020-02-14 15:51:46 +0000

    commit 2
    lwc start point
    apex framework change

---
+++ force-app/main/default/lwc/tMS_ComponentFC/tMS_ComponentFC.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import { LightningElement } from 'lwc';
+import executeApex from '@salesforce/apex/IServiceController.execute';
+
+export default class TMS_ComponentFC extends LightningElement {
+   
+   
+    hello(event) {
+        /*
+        console.log("in hello world ");
+        let requestParameters = {"record" : {"assigned_User__c": "user", "issue_Type__c": "Hardware", '+
+        ' "status__c": "New", "ticket_heading__c": "sample heading", "ticketDetails__c": "details"'+
+         '}}',TMS_WrapperClass.TicketWrapper.class}
+        let inputData ; 
+        executeApex(inputData).then(data => {
+            console.log("data "+ data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        this.setDefaultValues();  */
+    }
+}
