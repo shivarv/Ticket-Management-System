9314bf90d1e5b14aa9c58c70e77d6110c4196c14
shiva_rv <shivarv1993@gmail.com>
2020-02-14 16:50:57 +0000

    code changes

--- force-app/main/default/lwc/tMS_ComponentFC/tMS_ComponentFC.js
+++ force-app/main/default/lwc/tMS_ComponentFC/tMS_ComponentFC.js
@@ -1,23 +1,25 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import { LightningElement } from 'lwc';
 import executeApex from '@salesforce/apex/IServiceController.execute';
+import executeApexWithReturn from '@salesforce/apex/IServiceController.execute';
 
 export default class TMS_ComponentFC extends LightningElement {
    
    
     hello(event) {
-        /*
         console.log("in hello world ");
-        let requestParameters = {"record" : {"assigned_User__c": "user", "issue_Type__c": "Hardware", '+
-        ' "status__c": "New", "ticket_heading__c": "sample heading", "ticketDetails__c": "details"'+
-         '}}',TMS_WrapperClass.TicketWrapper.class}
-        let inputData ; 
-        executeApex(inputData).then(data => {
+
+        let requestParameters = {inputData : {assigned_User__c: "user", issue_Type__c: "Hardware", status__c: "New", ticket_heading__c: "sample heading", ticketDetails__c: "details"}, className : "TMS_WrapperClass.TicketWrapper"};
+
+         
+        let inputData = JSON.stringify(requestParameters); 
+        executeApexWithReturn(inputData).then(data => {
             console.log("data "+ data);
             })
             .catch(error => {
                 console.log(error);
             })
-        this.setDefaultValues();  */
+        this.setDefaultValues(); 
     }
 }
