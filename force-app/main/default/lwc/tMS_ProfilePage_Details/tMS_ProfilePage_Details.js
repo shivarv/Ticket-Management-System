4d578a8677e5bf1993a2943db6d0517849c51dd7
shiva_rv <shivarv1993@gmail.com>
2020-02-25 16:42:40 +0000

    code changes
    commit

--- force-app/main/default/lwc/tMS_ProfilePage_Details/tMS_ProfilePage_Details.js
+++ force-app/main/default/lwc/tMS_ProfilePage_Details/tMS_ProfilePage_Details.js
@@ -2,8 +2,8 @@ import { LightningElement, api } from 'lwc';
 
 export default class TMS_ProfilePage_Details extends LightningElement {
     @api userDetails;
-
     constructor() {
+        super();
         this.userDetails = {
             fullName: 'Shiva RV',
             email: 'shivarv@gmail.com',
