35cff655d0f4a6a93ce1e8dbd4f3634144405e3f
shiva_rv <shivarv1993@gmail.com>
2020-02-25 16:18:39 +0000

    code changes
    code update

--- force-app/main/default/lwc/tMS_ProfilePage_Details/tMS_ProfilePage_Details.js
+++ force-app/main/default/lwc/tMS_ProfilePage_Details/tMS_ProfilePage_Details.js
@@ -1,7 +1,6 @@
-import TMS_ComponentFC from 'c/tMS_ComponentFC';
 import { LightningElement, api } from 'lwc';
 
-export default class TMS_ProfilePage extends TMS_ComponentFC {
+export default class TMS_ProfilePage_Details extends LightningElement {
     @api userDetails;
 
     constructor() {
