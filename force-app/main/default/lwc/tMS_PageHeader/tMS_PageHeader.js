46062596c28fea40dca4465359d3a2981f67faeb
shiva <shivarv1993@gmail.com>
2020-02-25 21:08:48 +0000

    code change

--- force-app/main/default/lwc/tMS_PageHeader/tMS_PageHeader.js
+++ force-app/main/default/lwc/tMS_PageHeader/tMS_PageHeader.js
@@ -1,3 +1,10 @@
 import TMS_ComponentFC from "c/tMS_ComponentFC";
+import { LightningElement,api } from 'lwc';
 
-export default class TMS_PageHeader extends TMS_ComponentFC {}
+export default class TMS_PageHeader extends TMS_ComponentFC {
+    @api chosenTitle;
+    constructor() {
+        super();
+        this.chosenTitle = 'Dashboard';
+    }
+}
