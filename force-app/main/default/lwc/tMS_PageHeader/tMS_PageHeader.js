8b51aec98f214d9f6be6c66b4e24d6fc19dd6d1f
shiva <shivarv1993@gmail.com>
2020-02-25 20:48:41 +0000

    Revert "commit"
    This reverts commit adb85f89594c7d34e1de66f5eb39f9cb96958806.

--- force-app/main/default/lwc/tMS_PageHeader/tMS_PageHeader.js
+++ force-app/main/default/lwc/tMS_PageHeader/tMS_PageHeader.js
@@ -1,11 +1,3 @@
 import TMS_ComponentFC from "c/tMS_ComponentFC";
-import { LightningElement,api } from 'lwc';
-export default class TMS_PageHeader extends TMS_ComponentFC {
-@api chosenTitle;
 
-    constructor() {
-        super();
-        this.chosenTitle = 'Dashboard';
-    }
-
-}
+export default class TMS_PageHeader extends TMS_ComponentFC {}
