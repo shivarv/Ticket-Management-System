ceeea194df231d5c38f165044594c8bc50cc3889
shiva_rv <shivarv1993@gmail.com>
2020-02-28 11:03:11 +0000

    header profile page components commit
    code commit

---
+++ force-app/main/default/lwc/tMS_PageHeader_SearchExpanded/tMS_PageHeader_SearchExpanded.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class TMS_PageHeader_SearchExpanded extends LightningElement {
+    isSearchExpanded = false;
+
+
+}
---
+++ force-app/main/default/lwc/tMS_PageHeader_Setting/tMS_PageHeader_Setting.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class TMS_PageHeader_Setting extends LightningElement {}
