9260db275c7349e172ad522c3eb652f6ce0a3331
shiva_rv <shivarv1993@gmail.com>
2020-03-03 16:46:56 +0000

    code commit

--- force-app/main/default/lwc/tMS_ContentPage_Ticket/tMS_ContentPage_Ticket.js
+++ force-app/main/default/lwc/tMS_ContentPage_Ticket/tMS_ContentPage_Ticket.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class TMS_ContentPage_Ticket extends LightningElement {}
+export default class TMS_ContentPage_Ticket extends LightningElement {
+    @api contactName;
+    @api subject;
+    @api ticketStatus; // Open , Pending, Resolved, Closed, Waiting Customer Input
+    @api ticketPriotity; // Low, Medium, High, Urgent
+    @api currentTicketUpdate;//created date, closed date, overdue date , created 13days ago.overdue by 11days, closed 5days ago
+   
+    getFirstLetter() {
+
+    }
+}
---
+++ force-app/main/default/lwc/tMS_ContentPage_TicketList/tMS_ContentPage_TicketList.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class TMS_ContentPage_TicketList extends LightningElement {
+  
+    ticketList = [];
+    constructor() {
+        super();
+        this.currentTicketUpdate = [];
+        this.currentTicketUpdate.push({id : 1, contactName: 'Raj', subject: 'Connecting issue in laptop',
+            currentTicketUpdate : 'closed on time . resolution on time',
+            ticketStatus: 'Open',
+            ticketPriotity: 'Low'
+        });
+
+        this.currentTicketUpdate.push({id : 2 ,contactName: 'Vis', subject: 'New Keyboard',
+            currentTicketUpdate: 'Agent Responded .  Overdue by 8 days',
+            ticketStatus: 'Open',
+            ticketPriotity: 'Medium'
+        });
+    }
+
+}
